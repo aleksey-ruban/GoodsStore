831169e5655c09a2e38af464f0548fed5be978ff
aleksey-ruban <alekseyruban555@gmail.com>
2025-02-11 15:35:50 +0300

    Initial Commit

---
+++ GoodsStoreTests/GoodsStoreTests.swift
@@ -0,0 +1,17 @@
+//
+//  GoodsStoreTests.swift
+//  GoodsStoreTests
+//
+//  Created by Aleksey Ruban on 11.02.2025.
+//
+
+import Testing
+@testable import GoodsStore
+
+struct GoodsStoreTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
