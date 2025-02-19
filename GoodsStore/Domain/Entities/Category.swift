af2a4ad284ae36b1aa9a05b4556382a5fbc148e2
aleksey-ruban <alekseyruban555@gmail.com>
2025-02-20 00:46:01 +0300

    Domain layer - entities skeleton
    Added skeleton of domain layer entities

---
+++ GoodsStore/Domain/Entities/Category.swift
@@ -0,0 +1,8 @@
+//
+//  Category.swift
+//  GoodsStore
+//
+//  Created by Aleksey Ruban on 19.02.2025.
+//
+
+import Foundation
---
+++ GoodsStore/Domain/Entities/SearchQuery.swift
@@ -0,0 +1,8 @@
+//
+//  SearchQuery.swift
+//  GoodsStore
+//
+//  Created by Aleksey Ruban on 19.02.2025.
+//
+
+import Foundation
